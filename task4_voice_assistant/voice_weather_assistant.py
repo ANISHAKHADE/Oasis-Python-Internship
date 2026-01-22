f4f96b7ace31b278335079bfdf4c3ab9f50878a4
ANISHAKHADE <144530628+ANISHAKHADE@users.noreply.github.com>
2026-01-22 09:49:48 +0530

    Added Voice Assistant with Weather Integration

---
+++ task4_voice_assistant/voice_weather_assistant.py
@@ -0,0 +1,84 @@
+import speech_recognition as sr
+def stt():
+    r=sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Adjusting for background noise...")
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("Speak something now...")
+        audio= r.listen(source, timeout=5, phrase_time_limit=5)
+
+    print("Audio captured successfully...")
+
+    try:
+        return r.recognize_google(audio)
+        # print("Recognized :" , text)
+    except sr.UnknownValueError:
+        print("Could not understand audio") 
+    except sr.RequestError as e:
+        print("API error:", e)    
+
+ 
+
+import requests
+def basic_weather(city):
+    api_key="38639769932b2f7b724c6d72d650937c"
+    base_url=f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+    try:
+        response =requests.get(base_url, timeout=5)
+        response.raise_for_status()
+        return response.json() 
+    except requests.exceptions.RequestException:
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+text=stt()
+print("Recognized :" , text)
+if "weather" in text:
+    print("Weather command recognized ")
+    if "in" in text:
+        city = str(text.split("in")[1].strip())
+        data=basic_weather(city)
+        # print( data )
+        
+        if data is None:
+            print("No data received from API")
+        elif data['cod'] != 200: #str(data.get('cod'))
+            print("City not found...API error")
+        else:
+            city_name=data['name']
+            temp =data['main'] ['temp'] 
+            humidity = data ['main'] ['humidity']   
+            description = data['weather'][0]['description'] 
+            wind_speed = data['wind']['speed']     
+            
+
+            print("----- Weather Report -----")
+            print(f" City: {city_name} ")
+            print(f" Tempreture :{temp} °C ")
+            print(f" Humidity : {humidity}  ")
+            print(f" Description : {description} ")
+            print(f" Wind Speed : {wind_speed} m/s ")
+    else:
+        print("Please say: weather in <city>") 
+        city=None
+    # data =basic_weather(city)
+    
+elif "exit"in text or "quit" in text:
+    print("Exiting...")
+else:
+    print("Command not recognized ")
+
+
+    
